b205d4693c8fb4b30b0751d192a9e5d0f3e9cb49
Bekbol <bekbol.bv@gmail.com>
2026-01-20 10:53:18 +0500

    add yandex map

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,6 @@ import { BrowserRouter } from "react-router-dom";
 import App from "@/app/App";
 
 import "./app/styles/main.scss";
-import "leaflet/dist/leaflet.css";
 
 createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
--- src/pages/HomePage/ui/RestourantsMap/RestourantsMap.tsx
+++ src/pages/HomePage/ui/RestourantsMap/RestourantsMap.tsx
@@ -1,34 +1,70 @@
 import { Container } from "@/shared/ui/Container";
 import styles from "./RestourantsMap.module.scss";
-import { MapContainer, Marker, Popup } from "react-leaflet";
-import MarkerClusterGroup from "react-leaflet-cluster";
 
-type Restaurant = {
+import { YMaps, Map, Placemark } from "@pbe/react-yandex-maps";
+import useBreakpoint from "@/shared/lib/hooks/useBreakpoint";
+
+export interface Restaurant {
   id: number;
   name: string;
   lat: number;
-  lng: number;
-};
+  lon: number;
+}
 
 const restaurants: Restaurant[] = [
-  { id: 1, name: "Cafe A", lat: 41.311, lng: 69.24 },
-  { id: 2, name: "Cafe B", lat: 41.315, lng: 69.245 },
-  { id: 3, name: "Cafe C", lat: 41.309, lng: 69.238 },
+  {
+    id: 1,
+    name: "Burger House",
+    lat: 41.311081,
+    lon: 69.240562,
+  },
+  {
+    id: 2,
+    name: "Pizza Time",
+    lat: 41.3155,
+    lon: 69.245,
+  },
+  {
+    id: 3,
+    name: "Cafe Istanbul",
+    lat: 41.308,
+    lon: 69.235,
+  },
 ];
 
 function RestourantsMap() {
+  const { mobile } = useBreakpoint();
+
   return (
     <section className={styles.section}>
       <Container>
-        <MapContainer style={{ height: "400px" }} scrollWheelZoom={false}>
-          <MarkerClusterGroup>
-            {restaurants.map((r) => (
-              <Marker key={r.id} position={[r.lat, r.lng]}>
-                <Popup>{r.name}</Popup>
-              </Marker>
-            ))}
-          </MarkerClusterGroup>
-        </MapContainer>
+        <div className={styles.mapWrapper}>
+          <YMaps
+            query={{
+              apikey: "5a7dfd36-204d-4939-abea-b4b9011551d8",
+            }}
+            preload
+          >
+            <Map
+              defaultState={{
+                center: [41.311081, 69.240562],
+                zoom: 13,
+              }}
+              width="100%"
+              height={mobile ? "250px" : "500px"}
+            >
+              {restaurants.map((restaurant) => (
+                <Placemark
+                  key={restaurant.id}
+                  geometry={[restaurant.lat, restaurant.lon]}
+                  properties={{
+                    balloonContent: restaurant.name,
+                  }}
+                />
+              ))}
+            </Map>
+          </YMaps>
+        </div>
       </Container>
     </section>
   );
